b09b403631175ec41bbc5a8527353eeb3e06293d
Hi <147305531+Ismoil2888@users.noreply.github.com>
2023-10-29 06:52:11 +0500

    Update save_page.php

--- save_page.php
+++ save_page.php
@@ -1,1 +1,11 @@
+php
+<?php
+// Получаем данные из объекта FormData
+$pageNumber = $_POST['page'];
+$text = $_POST['text'];
 
+// Добавьте код для сохранения данных на сервере, например, в базу данных или в файл
+// Ниже приведен пример сохранения данных в файл
+$filename = 'page' . $pageNumber . '.txt';
+file_put_contents($filename, $text);
+?>
